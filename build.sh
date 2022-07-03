ba8d9262c8c24b3bface827e5e5ce601b5645635
leolivier <git@levillain.ml>
2022-07-03 13:39:28 +0200

    changed push option

--- build.sh
+++ build.sh
@@ -4,28 +4,29 @@ function usage() {
   echo $0 '[-h] [-p platform(s)] [-n] [-l]'
   echo "-p comma separated list of platforms. default=linux/amd64,linux/arm64,linux/arm/v7"
   echo "-n no cache"
-  echo "-l load only, don't push to docker registry"
+  echo "-u push to docker registry"
   echo "-h prints this help and exits"
   exit 1
 }
 
 PLATFORMS=""
 NOCACHE=""
-LOAD="--push"
+PUSH=""
 
-while getopts ":hp:i:nl" flag
+while getopts ":hp:i:nu" flag
 do
   case "${flag}" in
     h) usage;;
     p) PLATFORMS='--set *.platform='"${OPTARG}";;
     n) NOCACHE="--no-cache";;
-    l) LOAD="--load";;
+    u) PUSH="--push";;
     *) usage;;
   esac
 done
 
+PUSH=""
 cd $(dirname $0)
 #docker run --privileged --rm tonistiigi/binfmt:latest --install arm64,arm
 docker run --privileged --rm linuxkit/binfmt:v0.8
 set -x
-docker buildx bake -f ./build.hcl --progress plain $NOCACHE $LOAD $PLATFORMS
+docker buildx bake -f ./build.hcl --progress plain $NOCACHE $PUSH $PLATFORMS
